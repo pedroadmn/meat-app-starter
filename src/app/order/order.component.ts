f346678d579f80964d9b3d41220abb5a19a2b8eb
Pedro Adelino de Mello Neto <pedroadmn@gmail.com>
2017-12-15 20:58:18 -0300

    Personalized validated validator with reactive form

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -5,7 +5,7 @@ import { RadioOption } from './../shared/radio/radio-option.model';
 import { Component, OnInit } from '@angular/core';
 import { Order, OrderItem } from './order.model';
 
-import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -42,7 +42,21 @@ export class OrderComponent implements OnInit {
       number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('',[Validators.required])
-    });
+    }, {validator: OrderComponent.equalsTo});
+  }
+
+  static equalsTo(group: AbstractControl): {[key:string]: boolean} {
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+    if (!email || !emailConfirmation) {
+      return undefined;
+    }
+
+    if (email.value !== emailConfirmation.value) {
+      return { emailsNotMatch: true };
+    }
+
+    return undefined;
   }
 
   itemsValue(): number {
