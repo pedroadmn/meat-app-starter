7b197a0d11d93da04fbe66109ae6b24a09e25b9a
Pedro Adelino <pedro.mello@virtus.ufcg.edu.br>
2018-04-09 17:33:08 -0300

    isValid(User)

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from 'express';
+import { User, users } from './users';
 
 export const handleAuthentication = (req: Request, resp: Response) => {
-    const user = req.body;
+    const user: User = req.body;
     if (isValid(user)) {
-
+        const dbUser: User = users[user.email];
+        resp.json({name: dbUser.name, email: dbUser.email});
     } else {
-        resp.status(403).json({message: 'Dados inválidos'})
+        resp.status(403).json({message: 'Dados inválidos'});
     }
 
-    function isValid(user): boolean {
-        return false;
+    function isValid(user: User): boolean {
+        if (!user) {
+            return false;
+        }
+        const dbUser = users[user.email];
+        return dbUser !== undefined && dbUser.matches(user);
     }
 }
